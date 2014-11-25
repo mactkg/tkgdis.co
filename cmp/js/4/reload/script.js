92a64f920c546538325ef22c4db68235f78754b1
mactkg <mactkg@gmail.com>
2014-11-26 08:55:32 +0900

    remove debug out

--- cmp/js/4/reload/script.js
+++ cmp/js/4/reload/script.js
@@ -1,4 +1,3 @@
-console.log("okok");
 $.getJSON('http://twitcher.steer.me/user_timeline/nakamura?key=7ah6zjvj', function(data) {
   console.log(data);
   var html = $('#theTmpl').render(data[Math.floor(Math.random()*data.length)]);
